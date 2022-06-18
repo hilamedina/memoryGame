9b1f84f0b8e03f7598499b35e706c5351adbdba7
hilamedina <hm18986@gmail.com>
2022-06-18 19:51:36 +0300

    flip

--- src/component/card/Card.js
+++ src/component/card/Card.js
@@ -3,9 +3,18 @@ import './Card.css';
 
 const Card = ({ name, handleClick }) => {
   return (
-    <div className="card" onClick={handleClick}>
+    <div className="card">
+      {/* <div className="card" onClick={handleClick}> */}
+
       <div className="flip-card-inner">
-        <div class="flip-card-front"> {name}</div>
+        <div className="flip-card-front">
+          <img
+            src="https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/3:2/w_3329,h_2219,c_limit/1521-WIRED-Cat.jpeg"
+            alt="Avatar"
+            style={{ width: '140px', height: '140px' }}
+          />
+          {/* {name} */}
+        </div>
         <div className="flip-card-back">
           <h1>John Doe</h1>
           <p>Architect & Engineer</p>
